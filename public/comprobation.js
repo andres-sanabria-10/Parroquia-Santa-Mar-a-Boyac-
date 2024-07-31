1214e0f389b9917f9c103e17c2c05521df33ddd0
andres-sanabria-10 <andres.sanabria02@uptc.edu.co>
2024-07-31 18:50:52 -0500

    revision de token

--- public/comprobation.js
+++ public/comprobation.js
@@ -4,22 +4,32 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '/';
         return;
     }
-    const rolesPermitidos = ['Usuario', 'Admin', 'SuperAdmin'];
+    
     // Verificar el token con el backend
-    authenticatedFetch('https://api-parroquia.onrender.com/accessRole', {
+    authenticatedFetch('https://api-parroquia.onrender.com/accessRole/user', {
         method: 'GET'
     })
     
     .then(response => response.json())
     .then(data => {
         if (data.role !== 'Usuario') {
+            // Si el rol no es "Usuario", redirige al inicio o muestra un error
             throw new Error('Rol no autorizado');
         }
         // El token es válido y el rol es correcto, cargar el contenido de la página
-        loadUserContent();
+        loadUserContent(); // Asegúrate de tener definida esta función si quieres cargar contenido específico
     })
     .catch(error => {
         console.error('Error de autenticación:', error);
         window.location.href = '/';
     });
 });
+
+// Definición de la función authenticatedFetch
+function authenticatedFetch(url, options) {
+    const tokenSession = localStorage.getItem('tokenSession');
+    options.headers = options.headers || {};
+    options.headers['Authorization'] = `Bearer ${tokenSession}`;
+    
+    return fetch(url, options);
+}
