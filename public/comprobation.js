c42a80aa2a3ee21194695eee41aa95a78a033607
andres-sanabria-10 <andres.sanabria02@uptc.edu.co>
2024-07-31 18:15:58 -0500

    verificacion de token en las paginas

---
+++ public/comprobation.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tokenSession = localStorage.getItem('tokenSession');
+    if (!tokenSession) {
+        window.location.href = '/';
+        return;
+    }
+
+    // Verificar el token con el backend
+    authenticatedFetch('https://api-parroquia.onrender.com/accessRole', {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.role !== 'Usuario') {
+            throw new Error('Rol no autorizado');
+        }
+        // El token es válido y el rol es correcto, cargar el contenido de la página
+        loadUserContent();
+    })
+    .catch(error => {
+        console.error('Error de autenticación:', error);
+        window.location.href = '/';
+    });
+});
