e1fca6dd1fa10c3c59ca5ce063cbfc833d32b47b
TatisLesmes <tatis26lesmesmail.com>
2024-08-01 02:56:40 -0500

    configuracionSuperAdmin

---
+++ public/configuracionSuperAdmin.js
@@ -0,0 +1,166 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mainContent = document.getElementById('Principal');
+    const configuracionLink = document.getElementById('configSuperAdmin');
+    const inicioLink = document.getElementById('inicioLink');
+    const mobileinicioLink = document.getElementById('MobileinicioLink');
+
+
+    const contenidoInicial = `
+        <div class="container px-5 my-5 text-center">
+            <img src="/img/parroquiaSantaMaria.png" class="img-fluid" alt="">
+             <h1>Configuración</h1>
+             
+        </div>
+    `;
+
+
+
+    async function handleConfigClick(e) {
+        e.preventDefault();
+
+
+
+
+        try {
+            const response = await fetch(`https://api-parroquia.onrender.com/user/data`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error || 'Error al obtener los datos del perfil');
+            }
+
+            const data = await response.json();
+
+
+            console.log('Datos del perfil:', data);
+
+            const configHTML = `
+                <div class="container px-5 my-5 text-center">
+                    <h1>Perfil</h1>
+                    
+                    <div class="container">
+                        <div class="main-body">
+                            <div class="row gutters-sm">
+                                <div class="col-md-4 mb-3">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="d-flex flex-column align-items-center text-center">
+                                                <img src="${data.avatarUrl || 'https://bootdey.com/img/Content/avatar/avatar7.png'}" alt="Admin" class="rounded-circle" width="150">
+                                                <div class="mt-3">
+                                                    <h4>${data.name}</h4>
+                            
+                                                    <button class="btn btn-primary">Follow</button>
+                                                    <button class="btn btn-outline-primary">Message</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-8">
+                                    <div class="card mb-3">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <h6 class="mb-0">Nombre</h6>
+                                                </div>
+                                                <div class="col-sm-9 text-secondary">
+                                                    ${data.name}
+                                                </div>
+                                                <hr>
+                                                <div class="col-sm-3">
+                                                    <h6 class="mb-0">Apellido</h6>
+                                                </div>
+                                                <div class="col-sm-9 text-secondary">
+                                                    ${data.lastName}
+                                                </div>
+                                                <hr>
+                                                <div class="col-sm-3">
+                                                    <h6 class="mb-0">N° de documento</h6>
+                                                </div>
+                                                <div class="col-sm-9 text-secondary">
+                                                    ${data.documentNumber}
+                                                </div>
+                                                <hr>
+
+                                                <div class="col-sm-3">
+                                                    <h6 class="mb-0">Rol</h6>
+                                                </div>
+                                                <div class="col-sm-9 text-secondary">
+                                                    ${data.role}
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <h6 class="mb-0">Email</h6>
+                                                </div>
+                                                <div class="col-sm-9 text-secondary">
+                                                    ${data.mail}
+                                                </div>
+                                            </div>
+                                            <hr>
+                                           
+                                        
+                                           
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <a class="btn btn-info" target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            mainContent.innerHTML = configHTML;
+        } catch (error) {
+            console.error('Error al obtener datos del perfil:', error);
+            Swal.fire({
+                title: 'Error',
+                text: error.message,
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            });
+        }
+    }
+
+
+    function handleInicioClick(e) {
+        e.preventDefault();
+        mainContent.innerHTML = contenidoInicial;
+    }
+
+    const token = localStorage.getItem('tokenSession'); // Obtener el token de autorización
+
+    console.log(token);
+
+    if (configuracionLink) {
+        configuracionLink.addEventListener('click', handleConfigClick);
+    }
+
+
+
+    if (mobileinicioLink) {
+        mobileinicioLink.addEventListener('click', handleConfigClick);
+    }
+
+
+
+    if (inicioLink) {
+        inicioLink.addEventListener('click', handleInicioClick);
+    }
+
+
+});
